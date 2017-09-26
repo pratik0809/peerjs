b9f3a2df95a84f50196acbed93fae42d8ec4b5f8
Yani Mihaylov <ymihaylo@hawk.iit.edu>
2017-09-26 12:53:58 -0500

    Checkbox ticks sending

--- Manager/index.js
+++ Manager/index.js
@@ -4,15 +4,77 @@ peer.on('open', function(id) {
   document.getElementById("pid").innerText = id;
 });
 
-let conn = peer.connect('0oa3kkbfccgzaor');
+	let conn = peer.connect('d4vp2pdnqpzrrudi');
 
-conn.on('open', function(){
-  conn.send('hi!');
-});
+	function connect(){
+		conn.on('data', function(data) {
+    		console.log(data);
 
-peer.on('connection', function(conn) {
-  console.log(conn);
-  conn.on('data', function(data){
-    console.log(data);
-  });
-});
+        });
+	}
+
+	function check(res){
+		document.getElementById("checkbox").checked = res;
+	}
+
+	 conn.on('open', function() {
+        connect(conn);
+      });
+
+	peer.on('connection', function(conn) {
+	  	console.log(conn);
+	  	conn.on('data', function(data){
+	  		console.log(data);
+	  		check(data);
+	  	});
+
+	});
+
+	function submit(){
+	 	//conn.send(document.getElementById("message").value);
+	 	conn.send(document.getElementById("checkbox").checked);
+	 }
+
+
+	
+
+
+
+
+
+
+	/*peer.on('open')function(conn) {
+	  	console.log(conn);
+	  	console.log("conn ---");
+	  	conn.on('data', function(data){
+	  	console.log(data);
+	  		});
+		});
+	*/
+
+
+
+
+
+
+
+
+	/*conn.on('open', function(){
+  		conn.send('NOPE');
+ 		conn.send('wassup, fucker');
+ 	});*/
+
+	/*conn.on('open', function(){
+		message = document.getElementById("message").value;
+		conn.send('hi!');
+		conn.send('wassup, bietch');
+		conn.send(message);
+	});*/
+
+
+
+
+
+
+
+
